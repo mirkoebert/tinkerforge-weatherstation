2f5dd198cc2dc927fc1a97cfde774b9f28f5865c
Mirko Ebert <mirko.ebert@gmail.com>
2018-12-16 18:08:11 +0100

    fix daylight  problem in web interface

--- src/main/java/com/ebertp/DateX.java
+++ src/main/java/com/ebertp/DateX.java
@@ -4,13 +4,17 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 final class DateX {
 
     private static DateX instance = null;
-    private SimpleDateFormat sdf1 = new SimpleDateFormat("EE HH:mm:ss d. MMMM YYYY");
+    private ZoneId zoneId = ZoneId.of( "Europe/Berlin" );
     private boolean isOnline = false;
 
     private DateX() {
@@ -18,13 +22,16 @@ final class DateX {
     }
 
     String getDateString() {
+        ZonedDateTime zdt = ZonedDateTime.now( zoneId ) ;
+        log.info("QQQ: "+zdt);
+        log.info("QQQ: "+zdt.format(DateTimeFormatter.ofPattern("EE HH:mm:ss d. MMMM YYYY")));
         String r = "";
         if (isOnline) {
-            r = sdf1.format(new Date());
+            r = zdt.format(DateTimeFormatter.ofPattern("EE HH:mm:ss d. MMMM YYYY"));
         } else {
             isOnline = isOnline();
             if (isOnline) {
-                r = sdf1.format(new Date());
+                r = zdt.format(DateTimeFormatter.ofPattern("EE HH:mm:ss d. MMMM YYYY"));
             }
         }
         return r;
