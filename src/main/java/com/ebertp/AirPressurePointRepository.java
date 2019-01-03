4da9cf6df34da8ea228bc16d4dbae1ea43ebdd20
Mirko Ebert <mirko.ebert@gmail.com>
2019-01-03 21:14:58 +0100

    more Rest

---
+++ src/main/java/com/ebertp/AirPressurePointRepository.java
@@ -0,0 +1,51 @@
+package com.ebertp;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import lombok.Getter;
+
+public class AirPressurePointRepository {
+
+    @Getter
+    private List<AirpressurePoint> aplist = new ArrayList<AirpressurePoint>();
+    static final long H1_IN_MSEC = 60 * 60 * 1000;
+
+    @Getter
+    private static final AirPressurePointRepository INSTANCE = new AirPressurePointRepository();
+    
+    private void removeOldData() {
+        long now = System.currentTimeMillis();
+        for (Iterator<AirpressurePoint> iterator = aplist.iterator(); iterator.hasNext();) {
+            AirpressurePoint airpressurePoint = iterator.next();
+            if ((now - airpressurePoint.date) > H1_IN_MSEC) {
+                iterator.remove();
+            }
+        }
+    }
+    AirpressurePoint getMin(AirpressurePoint current) {
+        AirpressurePoint min = current;
+        for (AirpressurePoint airpressurePoint : aplist) {
+            if (airpressurePoint.airpressure < min.airpressure) {
+                min = airpressurePoint;
+            }
+        }
+        return min;
+    }
+
+    AirpressurePoint getMax(AirpressurePoint current) {
+        AirpressurePoint max = current;
+        for (AirpressurePoint airpressurePoint : aplist) {
+            if (airpressurePoint.airpressure > max.airpressure) {
+                max = airpressurePoint;
+            }
+        }
+        return max;
+    }
+    public void add(AirpressurePoint cap) {
+        removeOldData();
+        aplist.add(cap);
+    }
+
+}
