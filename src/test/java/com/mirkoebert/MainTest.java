9874efd946e0535bbac221b1dd0c563fd20414e8
Mirko Ebert <mirko.ebert@gmail.com>
2019-12-21 10:50:02 +0100

    add main test

---
+++ src/test/java/com/mirkoebert/MainTest.java
@@ -0,0 +1,20 @@
+package com.mirkoebert;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@ActiveProfiles("test")
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MainTest {
+
+	@Test
+	public void contextLoads() {
+	    // test loading full spring context
+	}
+
+}
+
