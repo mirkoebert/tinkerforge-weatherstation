d1583aad4b05b7d7cd219901bca53ad2586c64bb
Mirko Ebert <mirko.ebert@gmail.com>
2020-03-14 19:51:43 +0100

    add missing controler

---
+++ src/main/java/com/mirkoebert/controllerIn/WeatherStationWebInfoController.java
@@ -0,0 +1,80 @@
+package com.mirkoebert.controllerIn;
+
+import com.mirkoebert.weather.openweather.OpenWeatherService;
+import com.mirkoebert.weather.openweather.http.OpenWeatherWeatherStation;
+import com.mirkoebert.weather.openweather.http.Sender;
+import com.mirkoebert.weather.tinkerforge.TinkerforgeWeatherStation;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Lazy
+public class WeatherStationWebInfoController {
+
+    @Autowired
+    private TinkerforgeWeatherStation w;
+
+    @Autowired 
+    private Sender sender;
+
+    @Value("${info.app.name}")
+    private String applicationName;
+
+    @Value("${info.app.version}")
+    private String buildVersion;
+
+    @Value("${weatherstation.position.altitude}")
+    private String altitude;
+
+    @Value("${weatherstation.mode.nightmode}")
+    private String nightmode;
+
+    @Value("${weatherstation.mode.alarmfashing}")
+    private String alarmflashingmode;
+
+    @Value("${openweather.enable}")
+    private boolean openweather_enable;
+
+    @Value("${openweather.station_id}")
+    private String openweather_stationid;
+
+    @Autowired
+    private OpenWeatherService owss;
+  
+
+    @GetMapping("/info")
+    public String info(Map<String, Object> model) {
+        model.put("date", DateX.getInstance().getDateString());
+        model.put("startDate",  DateX.DATE_TIME_FORMATER.format(w.getStartDate()));
+        model.put("applicationName", applicationName);
+        model.put("buildVersion", buildVersion);
+        model.put("online", DateX.getInstance().isOnline());
+        model.put("altitude", altitude);
+        model.put("nightmode", nightmode);
+        model.put("alarmflashingmode", alarmflashingmode);
+
+        model.put("openweather_enable" , openweather_enable);
+        if(openweather_enable) {
+            model.put("openweather_stationid", openweather_stationid);
+
+            OpenWeatherWeatherStation ows = owss.getStation();
+            if (ows != null) {
+                model.put("openweather_name", ows.getName());
+                model.put("openweather_latitude", ows.getLatitude());
+                model.put("openweather_longitude", ows.getLongitude());
+            }
+
+            model.put("openweather_sendcount", sender.getSendCount());
+            model.put("openweather_senderrorcount", sender.getSendErrorCount());
+        }
+        return "info";
+    }
+
+
+}
