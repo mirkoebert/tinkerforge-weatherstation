261bdb3fd52d6517bfe3214c97ec92964df914a2
Mirko Ebert <mirko.ebert@gmail.com>
2020-12-08 21:37:47 +0100

    update to spring boot 2.4

--- src/test/java/com/mirkoebert/MainTest.java
+++ src/test/java/com/mirkoebert/MainTest.java
@@ -1,20 +1,16 @@
 package com.mirkoebert;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 
 @ActiveProfiles("test")
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class MainTest {
 
-	@Test
-	public void contextLoads() {
-	    // test loading full spring context
-	}
+    @Test
+    public void contextLoads() {
+        // test loading full spring context
+    }
 
 }
-
